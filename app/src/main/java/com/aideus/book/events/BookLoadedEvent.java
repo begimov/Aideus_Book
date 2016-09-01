0f7bb5c6d922130e323685ca569e8a56bc2f0adb
begimov <begimov@gmail.com>
2016-09-01 09:48:45 +0300

    refactoring

--- app/src/main/java/com/aideus/book/events/BookLoadedEvent.java
+++ app/src/main/java/com/aideus/book/events/BookLoadedEvent.java
@@ -6,7 +6,7 @@ public class BookLoadedEvent {
 
     private BookContents mContents = null;
 
-    public BookLoadedEvent(BookContents contents) {
+    public BookLoadedEvent(final BookContents contents) {
         mContents = contents;
     }
 
